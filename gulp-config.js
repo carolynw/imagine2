456eb356f6b2685e48f00ccf93210d2b2371f8b6
Mike McBain <mike.mcbain@outlook.com>
2017-03-24 14:34:52 +0000

    Updated sass to match main theme

---
+++ gulp-config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    // note: all paths MUST end in /
+    return {
+        themeProjectRoot: "./src/",
+        webProjectRoot: "./dist/",
+        testSiteRoot: "./TestSite/",
+        iisRoot: "",
+        contentFolder: "Content/",
+        buildConfiguration: "",
+        solutionName: "Sage.Imagine"
+    };
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,156 +1,207 @@
-/*global require */
-(function () {
-    "use strict";
+/// <binding BeforeBuild='rebuild' Clean='cleanOutputs' ProjectOpened='default' />
+/*global require, module */
 
-    var gulp = require("gulp"),
-        concat = require("gulp-concat-util"),
-        connect = require("gulp-connect"),
-        cssmin = require("gulp-cssmin"),
-        del = require("del"),
-        fileInclude = require("gulp-file-include"),
-        htmlhint = require("gulp-htmlhint"),
-        merge = require("gulp-merge"),
-        rename = require("gulp-rename"),
-        replace = require("gulp-replace"),
-        sass = require("gulp-sass"),
-        uglify = require("gulp-uglify"),
-        watch = require("gulp-watch");
+"use strict";
 
-    var vendorSrc = {
-        fonts: [
-            "node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}",
-            "node_modules/bootstrap/dist/fonts/**/*.{ttf,woff,woff2,eot,svg}",
-            "node_modules/slick-carousel/slick/fonts/**/*.{ttf,woff,woff2,eot,svg}"
-        ],
-        images: [
-            "node_modules/slick-carousel/slick/ajax-loader.gif"
-        ],
-        sass: [],
-        scripts: [
-            "node_modules/jquery/dist/jquery.js",
-            "node_modules/bootstrap/dist/js/bootstrap.js",
-            "node_modules/ScrollMagic/scrollmagic/uncompressed/ScrollMagic.js",
-            "node_modules/slick-carousel/slick/slick.js"
-        ]
-    };
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    connect = require("gulp-connect"),
+    del = require("del"),
+    fileInclude = require("gulp-file-include"),
+    htmlHint = require("gulp-htmlhint"),
+    merge = require("gulp-merge"),
+    rename = require("gulp-rename"),
+    replace = require("gulp-replace"),
+    sass = require("gulp-sass"),
+    sourceMaps = require("gulp-sourcemaps"), // Ensure other modules are supported - https://github.com/floridoo/gulp-sourcemaps/wiki/Plugins-with-gulp-sourcemaps-support
+    uglify = require("gulp-uglify"),
+    wrapJs = require("gulp-wrap-js");
 
-    var src = {
-        _root: "src/",
-        html: [
-            "src/html/**/*.html",
-            "!src/html/**/_*.html" // exclude partial HTML files which should be handled by fileInclude
-        ],
-        fonts: [
-            "src/fonts/**/*.{ttf,woff,woff2,eot,svg}"
-        ],
-        images: [
-            "src/images/**/*.{svg,gif,jpg,png,tif}"
-        ],
-        sass: [
-            "src/sass/sage.scss"
-        ],
-        scripts: [
-            "src/scripts/**/*.js"
-        ]
-    };
+var gulpConfig = {
+    themeProjectRoot: "./src/",
+    webProjectRoot: "./tmp/",
+    testSiteRoot: "./dist/",
+    contentFolder: "Content/"
+};
 
-    var out = {
-        _root: "dist/",
-        html: "html/",
-        fonts: "Content/fonts/",
-        images: "Content/images/",
-        scripts: "Content/scripts/",
-        styles: "Content/styles/"
-    };
+/*****************************
+ Sources
+ *****************************/
 
-    gulp.task("default", ["rebuild", "startLocalWebServer"]);
-    gulp.task("rebuild", ["cleanOutput", "compileSass", "compileScripts", "compileHtml", "copyFonts", "copyImages"]);
+var vendorSrc = {
+    fonts: [
+        "./node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}",
+        "./node_modules/bootstrap/dist/fonts/**/*.{ttf,woff,woff2,eot,svg}",
+        "./node_modules/slick-carousel/slick/fonts/**/*.{ttf,woff,woff2,eot,svg}"
+    ],
+    images: [
+        "./node_modules/slick-carousel/slick/ajax-loader.gif"
+    ],
+    sass: [],
+    scripts: [
+        "./node_modules/jquery/dist/jquery.js",
+        "./node_modules/bootstrap/dist/js/bootstrap.js",
+        "./node_modules/ScrollMagic/scrollmagic/uncompressed/ScrollMagic.js",
+        "./node_modules/slick-carousel/slick/slick.js"
+    ]
+};
 
-    gulp.task("cleanOutput", function () {
-        return del.sync(out._root + "**");
+var src = {
+    html: [
+        gulpConfig.themeProjectRoot + "html/**/*.html",
+        "!" + gulpConfig.themeProjectRoot + "html/**/_*.html" // excludes 'partial' HTML files which should be handled by fileInclude
+    ],
+    fonts: [
+        gulpConfig.themeProjectRoot + "fonts/**/*.{ttf,woff,woff2,eot,svg}"
+    ],
+    images: [
+        gulpConfig.themeProjectRoot + "images/**/*.{svg,gif,jpg,png,tif}"
+    ],
+    sass: [
+        gulpConfig.themeProjectRoot + "sass/sage.scss",
+        gulpConfig.themeProjectRoot + "sass/htmlEditor.scss"
+    ],
+    scripts: [
+        gulpConfig.themeProjectRoot + "scripts/framework/**/*.js", // important: must be first due to load-order concerns
+        gulpConfig.themeProjectRoot + "scripts/**/*.js",
+        "!" + gulpConfig.themeProjectRoot + "scripts/**/*.test.js" // exclude unit tests
+    ]
+};
+
+/*****************************
+ Frontend Test Site
+ *****************************/
+
+gulp.task("TestSite-Server", ["TestSite-All"], function () {
+    gulp.watch([gulpConfig.themeProjectRoot + "html/**/*.html"], ["TestSite-Html", refreshWebServer]);
+    gulp.watch([vendorSrc.fonts, src.fonts], ["TestSite-Fonts", refreshWebServer]);
+    gulp.watch([vendorSrc.images, src.images], ["TestSite-Images", refreshWebServer]);
+    gulp.watch([gulpConfig.themeProjectRoot + "sass/**/*.scss"], ["TestSite-Sass", refreshWebServer]);
+    gulp.watch([vendorSrc.scripts, src.scripts], ["TestSite-Scripts", refreshWebServer]);
+
+    function refreshWebServer() {
+        return gulp.src(gulpConfig.webProjectRoot)
+            .pipe(connect.reload());
+    }
+
+    return connect.server({
+        port: 8888,
+        root: gulpConfig.testSiteRoot,
+        livereload: true
     });
+});
 
-    gulp.task("compileSass", function () {
-        return gulp.src(src.sass)
-            .pipe(sass().on("error", sass.logError))
-            .pipe(cssmin())
+gulp.task("TestSite-All", ["TestSite-Clean", "Content-All", "TestSite-Fonts", "TestSite-Html", "TestSite-Images", "TestSite-Sass", "TestSite-Scripts"]);
+
+gulp.task("TestSite-Clean", function () {
+    // del.sync ensures del completes before other tasks are run
+    return del.sync([gulpConfig.testSiteRoot + "**"]);
+});
+
+gulp.task("TestSite-Fonts", ["Content-Fonts"], function () {
+    return gulp.src(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "fonts/**")
+        .pipe(gulp.dest(gulpConfig.testSiteRoot + gulpConfig.contentFolder + "fonts/"));
+});
+
+gulp.task("TestSite-Html", function () {
+    return merge(copyRedirect(), compileHtml());
+
+    function copyRedirect() {
+        return gulp.src(gulpConfig.themeProjectRoot + "html/index.html")
+            .pipe(gulp.dest(gulpConfig.testSiteRoot));
+    }
+
+    function compileHtml() {
+        return gulp.src(src.html)
+            .pipe(fileInclude({prefix: "@@", basepath: "@file"})) // note that fileInclude doesn't directly support nested @@include statements
+            .pipe(fileInclude({prefix: "@@", basepath: "@file"})) // so we run it twice to process the first nested level
+            .pipe(replace(/[\u200B-\u200D\uFEFF]/g, "")) // strip out file BOMs left by fileInclude as it leads to whitespace issues
+            .pipe(htmlHint("./htmlhint.json"))
+            .pipe(htmlHint.reporter())
+            .pipe(gulp.dest(gulpConfig.testSiteRoot + "html"));
+    }
+});
+
+gulp.task("TestSite-Images", ["Content-Images"], function () {
+    return gulp.src(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "images/**")
+        .pipe(gulp.dest(gulpConfig.testSiteRoot + gulpConfig.contentFolder + "images/"));
+});
+
+gulp.task("TestSite-Sass", ["Content-Sass"], function () {
+    return gulp.src(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "styles/**")
+        .pipe(gulp.dest(gulpConfig.testSiteRoot + gulpConfig.contentFolder + "styles/"));
+});
+
+gulp.task("TestSite-Scripts", ["Content-Scripts"], function () {
+    return gulp.src(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "scripts/**")
+        .pipe(gulp.dest(gulpConfig.testSiteRoot + gulpConfig.contentFolder + "scripts/"));
+});
+
+/*****************************
+ Common Content
+ *****************************/
+
+gulp.task("Content-All", ["Content-Clean", "Content-Fonts", "Content-Images", "Content-Sass", "Content-Scripts"]);
+
+gulp.task("Content-Clean", function () {
+    // del.sync ensures del completes before other tasks are run
+    return del.sync([gulpConfig.webProjectRoot + gulpConfig.contentFolder + "**"]);
+});
+
+gulp.task("Content-Fonts", function () {
+    return gulp.src(vendorSrc.fonts.concat(src.fonts))
+        .pipe(gulp.dest(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "fonts"));
+});
+
+gulp.task("Content-Images", function () {
+    return gulp.src(vendorSrc.images.concat(src.images))
+        .pipe(gulp.dest(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "images"));
+});
+
+gulp.task("Content-Sass", function () {
+    return gulp.src(src.sass)
+        .pipe(sourceMaps.init())
+        .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(sourceMaps.write("./", {mapSources: mapSources}))
+        .pipe(gulp.dest(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "styles"));
+
+    function mapSources(sourcePath) {
+        return "/" + gulpConfig.themeProjectRoot + "/sass/" + sourcePath;
+    }
+});
+
+gulp.task("Content-Scripts", function () {
+    return merge(compileVendorScripts(), compileScripts())
+        .pipe(gulp.dest(gulpConfig.webProjectRoot + gulpConfig.contentFolder + "scripts"));
+
+    function compileVendorScripts() {
+        return gulp.src(vendorSrc.scripts, {base: "./node_modules"})
+            .pipe(sourceMaps.init())
+            .pipe(concat("sage.vendor.js"))
+            .pipe(uglify())
             .pipe(rename({suffix: ".min"}))
-            .pipe(gulp.dest(out._root + out.styles));
-    });
+            .pipe(sourceMaps.write("./", {mapSources: mapSources}));
 
-    gulp.task("compileScripts", function () {
-        return merge(compileVendorScripts(), compileScripts())
-            .pipe(gulp.dest(out._root + out.scripts));
-
-        function compileVendorScripts() {
-            return gulp.src(vendorSrc.scripts)
-                .pipe(concat("sage.vendor.js"))
-                .pipe(uglify())
-                .pipe(rename({suffix: ".min"}));
+        function mapSources(sourcePath) {
+            return "/node_modules/" + sourcePath;
         }
+    }
 
-        function compileScripts() {
-            // The concats wrap the entire output .js in a single "use strict" statement and removes the redundant nested ones
-            return gulp.src(src.scripts)
-                .pipe(concat("sage.js", {process: stripUseStrictStatements}))
-                .pipe(concat.header("(function() { \n  \"use strict\";\n\n"))
-                .pipe(concat.footer("\n}());"))
-                // .pipe(uglify())
-                .pipe(rename({
-                    suffix: ".min"
-                }));
+    function compileScripts() {
+        return gulp.src(src.scripts, {base: "./"})
+            .pipe(sourceMaps.init())
+            .pipe(concat("sage.js"))
+            // Optimization: wrap in self-executing function with common globals/settings passed in for better minification
+            .pipe(wrapJs("(function(window) { \n  \"use strict\";\n\n%= body %\n}(window));"))
+            .pipe(uglify())
+            .pipe(rename({
+                suffix: ".min"
+            }))
+            .pipe(sourceMaps.write("./", {mapSources: mapSources}));
 
-            function stripUseStrictStatements(src) {
-                return src.replace(/\s*(?:"|')use strict(?:"|');\s*\n/g, "\n");
-            }
+        function mapSources(sourcePath) {
+            return "/" + sourcePath;
         }
-    });
-
-    gulp.task("compileHtml", function () {
-        return merge(copyRedirect(), compileHtml());
-
-        function copyRedirect() {
-            return gulp.src(src._root + "html/index.html")
-                .pipe(gulp.dest(out._root));
-        }
-
-        function compileHtml() {
-            return gulp.src(src.html)
-                .pipe(fileInclude({prefix: "@@", basepath: "@file"})) // note that fileInclude doesn't directly support nested @@include statements
-                .pipe(fileInclude({prefix: "@@", basepath: "@file"})) // so we run it twice to process the first nested level
-                .pipe(replace(/[\u200B-\u200D\uFEFF]/g, "")) // strip out file BOMs left by fileInclude as it leads to whitespace issues
-                .pipe(htmlhint("./htmlhint.json"))
-                .pipe(htmlhint.reporter())
-                .pipe(gulp.dest(out._root + out.html));
-        }
-    });
-
-    gulp.task("copyFonts", function () {
-        return gulp.src(vendorSrc.fonts.concat(src.fonts))
-            .pipe(gulp.dest(out._root + out.fonts));
-    });
-
-    gulp.task("copyImages", function () {
-        return gulp.src(vendorSrc.images.concat(src.images))
-            .pipe(gulp.dest(out._root + out.images));
-    });
-
-    gulp.task("startLocalWebServer", function () {
-        gulp.watch([src._root + "html/**/*.html"], ["compileHtml", refreshWebServer]);
-        gulp.watch([src._root + "sass/**/*.scss"], ["compileSass", refreshWebServer]);
-        gulp.watch([vendorSrc.scripts, src.scripts], ["compileScripts", refreshWebServer]);
-        gulp.watch([vendorSrc.images, src.images], ["copyImages", refreshWebServer]);
-        gulp.watch([vendorSrc.fonts, src.fonts], ["copyFonts", refreshWebServer]);
-
-        function refreshWebServer() {
-            return gulp.src(out._root)
-                .pipe(connect.reload());
-        }
-
-        return connect.server({
-            port: 8888,
-            root: out._root,
-            livereload: true
-        });
-    });
-}());
+    }
+});
